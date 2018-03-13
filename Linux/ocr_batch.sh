a591b3c1e73941ec69b78526cfd99810f02e4270
Sreeharsha Poluru <sreeharshapoluru@gmail.com>
2018-03-12 21:35:50 -0400

    Completed batch OCR with bash commands - tested on 100+ files

--- Linux/ocr_batch.sh
+++ Linux/ocr_batch.sh
@@ -1,4 +1,3 @@
-
 for file in ../sample_small/*.tif
 do
 echo "Conversion start..."
@@ -6,14 +5,16 @@ echo "Conversion start..."
 convert $file temp.pbm
 convert temp.pbm temp.tif
 tesseract temp.tif output
+cat output.txt | tr -d " \t\n\r" > out.txt 
 rm temp.tif	
 rm temp.pbm
+rm output.txt
 
 echo "Conversion end..."
 
 echo "OCR start..."
 
-egrep 'R(e|E)(g|G)\. N(o|O)\. [0-9],[0-9]{3},[0-9]{3}$' output.txt >>  extracted.txt
+egrep -oh 'R(e|E)(g|G)[\.,]N(o|O)[\.,][0-9],[0-9]{3},[0-9]{3}' out.txt >> extracted.txt
 
 echo "OCR end..."
 
--- Linux/ocr_single.sh
+++ Linux/ocr_single.sh
@@ -1,15 +1,15 @@
 convert *.tif temp.pbm
 convert temp.pbm temp.tif
 tesseract temp.tif output
-cat output.txt | tr -d " \t\n\r" > out.txt
+cat output.txt | tr -d " \t\n\r" > out.txt 
 rm temp.tif	
 rm temp.pbm
-rm output.txt
+rm output
+rm out
 
 echo "Conversion end..."
 
-echo "OCR start..."
 
-egrep '^R(e|E)(g|G)(\.|,)N(o|O)(\.|,)[0-9],[0-9]{3},[0-9]{3}$' out.txt >>  extracted.txt
+egrep -oh 'R(e|E)(g|G)[\.,]N(o|O)[\.,][0-9],[0-9]{3},[0-9]{3}' out.txt >> extracted.txt
 
 echo "OCR end..."
