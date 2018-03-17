05d5715c985736e30bcd829767100ded57d4e062
Sreeharsha Poluru <sreeharshapoluru@gmail.com>
2018-03-17 16:30:10 -0400

    OCR using bash commands for a single and batch of files - tested on 100+ inputs

--- Linux/ocr_batch.sh
+++ Linux/ocr_batch.sh
@@ -1,21 +1,24 @@
 for file in ../sample_small/*.tif
 do
 echo "Conversion start..."
-
+# removing the aplha layer from the tiff images
 convert $file temp.pbm
 convert temp.pbm temp.tif
+# using tessearct to convert tiff to text
 tesseract temp.tif output
+#removing all the spaces from the text
 cat output.txt | tr -d " \t\n\r" > out.txt 
+#remove temporary files
 rm temp.tif	
 rm temp.pbm
 rm output.txt
-
 echo "Conversion end..."
 
 echo "OCR start..."
-
-egrep -oh 'R(e|E)(g|G)[\.,]N(o|O)[\.,][0-9],[0-9]{3},[0-9]{3}' out.txt >> extracted.txt
-
+# using grep search for the pattern
+egrep -oh 'R(e|E)(g|G)[\.,]N(o|O)[\.,][0-9],[0-9]{3},[0-9]{3}' out.txt >> extracted_regnums.txt
+#remove temp files
+rm out
 echo "OCR end..."
 
 done
--- Linux/ocr_single.sh
+++ Linux/ocr_single.sh
@@ -1,15 +1,20 @@
+echo "Conversion start..."
+# removing the aplha layer from the tiff images
 convert *.tif temp.pbm
 convert temp.pbm temp.tif
+# using tessearct to convert tiff to text
 tesseract temp.tif output
+#removing all the spaces from the text
 cat output.txt | tr -d " \t\n\r" > out.txt 
+#remove temporary files
 rm temp.tif	
 rm temp.pbm
 rm output
-rm out
-
 echo "Conversion end..."
 
-
-egrep -oh 'R(e|E)(g|G)[\.,]N(o|O)[\.,][0-9],[0-9]{3},[0-9]{3}' out.txt >> extracted.txt
-
+echo "OCR start..."
+# using grep search for the pattern
+egrep -oh 'R(e|E)(g|G)[\.,]N(o|O)[\.,][0-9],[0-9]{3},[0-9]{3}' out.txt >> extractedregnums.txt
+#remove temp files
+rm out
 echo "OCR end..."
