be63fa22103b6d35429c56cac0056ea4bee5c3e3
Sreeharsha Poluru <sreeharshapoluru@gmail.com>
2018-03-19 12:26:48 -0400

    OCR with Python - single files and batch of files  - cannot handle some formats - needs fixing

--- Python/ocr_batch.py
+++ Python/ocr_batch.py
@@ -1,33 +1,45 @@
-import pytesseract;
-import sys;
-from PIL import Image;
-import re;
-import os;
-import csv;
+import pytesseract
+import sys
+from PIL import Image
+import re
+import os
+import csv
 
 
-img = sys.argv[1]
- 
-# use tesseract convert tiff to .txt file
-textFile = open('output.txt','w');
-text = pytesseract.image_to_string(Image.open(img));
-textFile.write(text.encode('utf-8'));
-textFile.close();
+# read files from the directory and perform the OCR operation on them
+for files in sorted(os.listdir("../sample_small")):
+    # convert tiff file to a grayscale tiff file
+    # TODO get the img here!!!!
+    cmd1 = "convert ../sample_small/"+files+" temp.pbm"
+    cmd2 = "convert temp.pbm temp.tif"
+    os.system(cmd1)
+    os.system(cmd2)
 
-# use the regex to search for the text format
-tempFile = open('output.txt','r');
-temp = tempFile.read();
-tempFile.close();
-toSearch = ''.join(temp.split());
-regExpression = "R(e|E)(g|G)[\.,]N(o|O)[\.,][0-9],[0-9]{3},[0-9]{3}";
-tempNum = re.search(regExpression,toSearch,re.IGNORECASE).group();
-regNum = re.search("[0-9],[0-9]{3},[0-9]{3}",tempNum,re.IGNORECASE).group();
+    # use tesseract convert tiff to .txt file
+    textFile = open('output.txt','w')
+    text = pytesseract.image_to_string(Image.open("temp.tif"))
+    textFile.write(text.encode('utf-8'))
+    textFile.close()
 
-# output each filename and the corresponding extracted quantity into a csv file.
-with open("regnum.csv","a") as csvFile:
-    csvFileWriter = csv.writer(csvFile,dialect="excel");
-    csvFileWriter.writerow([img,regNum]);
-csvFile.close();
+    # use the regex to search for the text format
+    tempFile = open('output.txt','r')
+    temp = tempFile.read()
+    tempFile.close()
+    toSearch = ''.join(temp.split())
+    regExpression = "R(e|E)(g|G).N(o|O).[0-9].*[0-9][0-9][0-9].*[0-9][0-9][0-9]"
+    tempNum = re.search(regExpression,toSearch,re.IGNORECASE).group()
+    regNum = re.search("[0-9].*[0-9][0-9][0-9].*[0-9][0-9][0-9]",tempNum,re.IGNORECASE).group()
 
-# remove all extra stuff you created...close all files.....etc.
-os.remove('output.txt');
+    # output each filename and the corresponding extracted quantity into a csv file.
+    with open("regnum.csv","a") as csvFile:
+        csvFileWriter = csv.writer(csvFile,dialect="excel")
+        csvFileWriter.writerow([files,regNum])
+    csvFile.close()
+
+    # remove all extra stuff created...close all files.....etc.
+    os.remove('output.txt')
+    os.remove("temp.tif")
+    os.remove("temp.pbm")
+    
+
+print "DONE!!!"
--- Python/ocr_single.py
+++ Python/ocr_single.py
@@ -1,33 +1,36 @@
-import pytesseract;
-import sys;
-from PIL import Image;
-import re;
-import os;
-import csv;
+import pytesseract
+import sys
+from PIL import Image
+import re
+import os
+import csv
 
-
+#read file from the arguments
 img = sys.argv[1]
+cmd1 = "convert "+ img +" temp.pbm"
+cmd2 = "convert temp.pbm temp.tif"
+os.system(cmd1)
+os.system(cmd2)
  
 # use tesseract convert tiff to .txt file
-textFile = open('output.txt','w');
-text = pytesseract.image_to_string(Image.open(img));
-textFile.write(text.encode('utf-8'));
-textFile.close();
+textFile = open('output.txt','w')
+text = pytesseract.image_to_string(Image.open("temp.tif"))
+textFile.write(text.encode('utf-8'))
+textFile.close()
 
 # use the regex to search for the text format
-tempFile = open('output.txt','r');
-temp = tempFile.read();
-tempFile.close();
-toSearch = ''.join(temp.split());
-regExpression = "R(e|E)(g|G)[\.,]N(o|O)[\.,][0-9],[0-9]{3},[0-9]{3}";
-tempNum = re.search(regExpression,toSearch,re.IGNORECASE).group();
-regNum = re.search("[0-9],[0-9]{3},[0-9]{3}",tempNum,re.IGNORECASE).group();
+tempFile = open('output.txt','r')
+temp = tempFile.read()
+tempFile.close()
+toSearch = ''.join(temp.split())
+regExpression = "reg.no.[0-9].[0-9]{3}.[0-9]{3}"
+print toSearch.lower()
+tempNum = re.search(regExpression,toSearch.lower(),re.DOTALL).group()
+regNum = re.search("[0-9].[0-9]{3}.[0-9]{3}",tempNum.lower(),re.DOTALL).group()
 
-# output each filename and the corresponding extracted quantity into a csv file.
-with open("regnum.csv","a") as csvFile:
-    csvFileWriter = csv.writer(csvFile,dialect="excel");
-    csvFileWriter.writerow([img,regNum]);
-csvFile.close();
-
+print tempNum
+print regNum
 # remove all extra stuff you created...close all files.....etc.
-os.remove('output.txt');
+os.remove('output.txt')
+os.remove("temp.tif")
+os.remove("temp.pbm")
